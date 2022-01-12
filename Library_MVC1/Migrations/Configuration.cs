b397e6f1b245d3fb82049f77546f22dc70ac88e4
JamesOmare <jamesomare922@gmail.com>
2022-01-12 17:07:04 +0300

    Add project files.

---
+++ Library_MVC1/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Library_MVC1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Library_MVC1.Models.LibraryDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Library_MVC1.Models.LibraryDBContext";
+        }
+
+        protected override void Seed(Library_MVC1.Models.LibraryDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Library_MVC1/Models/Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Library_MVC1.Models
+{
+    public class Library
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        //public string Author { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class LibraryDBContext : DbContext
+    {
+        public DbSet<Library> Libraries { get; set; }
+    }
+
+}
